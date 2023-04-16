b5b81ea4c6ef49af9babfd95463ca8ca1ec5b7a4
Co4UH9IU_XAN <eugenkunin@gmail.com>
2023-04-16 13:03:11 +0300

    transfer commit

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,8 +2,15 @@ const urlUsers = 'http://localhost:8080/api/users'
 const insert = document.querySelector('#tableInsert')
 let result = ''
 
-const deleteModal  = new bootstrap.Modal(document.getElementById('deleteModal'))
-
+//const deleteModal  = new bootstrap.Modal(document.getElementById('deleteModal'))
+const editModal  = new bootstrap.Modal(document.getElementById('editModal'))
+const editModalForm = document.querySelector('editModalForm')
+const idEditModal = document.querySelector('idEditModal')
+const usernameEditModal = document.querySelector('usernameEditModal')
+const surnameEditModal = document.querySelector('surnameEditModal')
+const ageEditModal = document.querySelector('ageEditModal')
+const emailEditModal = document.querySelector('emailEditModal')
+const roleEditModal = document.querySelector('roleEditModal')
 
 
 // Заполнение таблицы юзерами
@@ -22,7 +29,7 @@ const showUsers = (users) => {
                     <td>${user.age}</td>
                     <td>${user.email}</td>
                     <td>${user.roles[0].name}</td>
-                    <td class = "text-center"><a class = "btnEdit btn btn-primary">Edit</a></td>
+                    <td class="text-center"><a class="btnEdit btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal">Edit</a></td>
                     <td class = "text-center"><a class = "btnDelete btn btn-danger">Delete</a></td>
                 </tr>
         `
@@ -44,11 +51,39 @@ catchButtons(document, 'click', '.btnDelete', e => {
 
     // тут помещяем в переменню строку с данными юзера и далее в id выдергиваем айдишник
     const dataString = e.target.parentNode.parentNode
-    const id = dataString.firstElementChild.innerHTML
-    console.log(id)
+
+    // ЗАхватываем поля юзера
+    const idForm = dataString.firstElementChild.innerHTML
+    const usernameForm = dataString.children[1].innerHTML
+    const surnameForm = dataString.children[2].innerHTML
+    const ageForm = dataString.children[3].innerHTML
+    const emailForm = dataString.children[4].innerHTML
+    const roleForm = dataString.children[5].innerHTML
+    console.log(idForm)
+    console.log(usernameForm)
+    console.log(surnameForm)
+    console.log(ageForm)
+    console.log(emailForm)
+    console.log(roleForm)
+    console.log("YдОляй!!!")
 })
 
 // Обработчик событий кнопки редактировать
 catchButtons(document, 'click', '.btnEdit', e => {
+    const dataString = e.target.parentNode.parentNode
+
+    // ЗАхватываем поля юзера
+    const idForm = dataString.firstElementChild.innerHTML
+    const usernameForm = dataString.children[1].innerHTML
+    const surnameForm = dataString.children[2].innerHTML
+    const ageForm = dataString.children[3].innerHTML
+    const emailForm = dataString.children[4].innerHTML
+    const roleForm = dataString.children[5].innerHTML
+    console.log(idForm)
+    console.log(usernameForm)
+    console.log(surnameForm)
+    console.log(ageForm)
+    console.log(emailForm)
+    console.log(roleForm)
     console.log("Не удОляй!!!")
 })
