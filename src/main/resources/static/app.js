574d39f14d6e0ee62ea9ae9f753127bb45da410b
Co4uH9lu-XAH <eugenkunin@gmail.com>
2023-04-17 11:13:58 +0300

    recreate edit and newUser modal

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,15 +2,6 @@ const urlUsers = 'http://localhost:8080/api/users'
 const insert = document.querySelector('#tableInsert')
 let result = ''
 
-//const deleteModal  = new bootstrap.Modal(document.getElementById('deleteModal'))
-const editModal  = new bootstrap.Modal(document.getElementById('editModal'))
-const editModalForm = document.querySelector('editModalForm')
-const idEditModal = document.querySelector('idEditModal')
-const usernameEditModal = document.querySelector('usernameEditModal')
-const surnameEditModal = document.querySelector('surnameEditModal')
-const ageEditModal = document.querySelector('ageEditModal')
-const emailEditModal = document.querySelector('emailEditModal')
-const roleEditModal = document.querySelector('roleEditModal')
 
 
 // Заполнение таблицы юзерами
@@ -29,7 +20,7 @@ const showUsers = (users) => {
                     <td>${user.age}</td>
                     <td>${user.email}</td>
                     <td>${user.roles[0].name}</td>
-                    <td class="text-center"><a class="btnEdit btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal">Edit</a></td>
+                    <td class="text-center"><a class="btnEdit btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalElement">Edit</a></td>
                     <td class = "text-center"><a class = "btnDelete btn btn-danger">Delete</a></td>
                 </tr>
         `
@@ -67,66 +58,3 @@ catchButtons(document, 'click', '.btnDelete', e => {
 
 })
 
-// Открытие и заполнение формы редактирования
-catchButtons(document, 'click', '.btnEdit', e => {
-    const dataString = e.target.parentNode.parentNode
-
-    // ЗАхватываем поля юзера
-    const idForm = dataString.firstElementChild.innerHTML
-    const usernameForm = dataString.children[1].innerHTML
-    const surnameForm = dataString.children[2].innerHTML
-    const ageForm = dataString.children[3].innerHTML
-    const emailForm = dataString.children[4].innerHTML
-    const roleForm = dataString.children[5].innerHTML
-
-    // Заполняем форму
-    var idEditModal = document.getElementById('idEditModal');
-    idEditModal.value = idForm;
-    var usernameEditModal = document.getElementById('usernameEditModal');
-    usernameEditModal.value = usernameForm;
-    var surnameEditModal = document.getElementById('surnameEditModal');
-    surnameEditModal.value = surnameForm;
-    var ageEditModal = document.getElementById('ageEditModal');
-    ageEditModal.value = ageForm;
-    var emailEditModal = document.getElementById('emailEditModal');
-    emailEditModal.value = emailForm;
-    //editModal.show()
-})
-
-//Обработчик события редактирования
-
-function editUser() {
-    editModalForm.addEventListener('submit', event => {
-        event.preventDefault()
-        let editedUserRoles = []
-        for (let i = 0; i < editModalForm.roles.options.length; i++) {
-            if (editModalForm.roles.options[i].selected) {
-                editedUserRoles.push({
-                    id: editModalForm.roles.options[i].value,
-                    authority: 'ROLE_' + editModalForm.roles.options[i].text
-                })
-            }
-        }
-
-        fetch('/api/users', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                id: editModalForm.id.value,
-                roles: editedUserRoles,
-                username: username.value,
-                surname: surname.value,
-                age: age.value,
-                email: email.value,
-                password: password.value
-            })
-        }).then (response => response.json())
-            .then(data => {
-                const newUsers = []
-                newUsers.push(data)
-            })
-
-        })
-    }
