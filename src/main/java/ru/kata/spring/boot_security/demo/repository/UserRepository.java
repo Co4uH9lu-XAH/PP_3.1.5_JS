84f7cc4e9a9a69082a6c7cb85f0feb544b35cdb7
Co4UH9IU_XAN <eugenkunin@gmail.com>
2023-04-18 01:40:52 +0300

    for check 2

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.models.User;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.eMail = :eMail")
