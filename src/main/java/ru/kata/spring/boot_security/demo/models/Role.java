9c40e726ee9c7b5d4d2a783f0937933278bb57f5
Co4uH9lu-XAH <eugenkunin@gmail.com>
2023-04-21 20:27:30 +0300

    created unoDirectional

--- src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
@@ -5,81 +5,61 @@ import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "role")
 public class Role implements GrantedAuthority {
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-@Column(name = "authority")
-private String authority;
+    @Column(name = "authority")
+    private String authority;
 
-@JsonIgnore
-@ManyToMany(mappedBy = "roles")
-    List<User> users;
+    public Role() {
+    }
 
-public Role() {
-        }
-
-public Role(String authority) {
+    public Role(String authority) {
         super();
         this.authority = authority;
-        }
+    }
 
-public String getAuthority() {
+    public String getAuthority() {
         return authority;
-        }
+    }
 
-public Long getId() {
+    public Long getId() {
         return id;
-        }
+    }
 
-public void setId(Long id) {
+    public void setId(Long id) {
         this.id = id;
-        }
+    }
 
-public void setAuthority(String authority) {
+    public void setAuthority(String authority) {
         this.authority = authority;
-        }
+    }
 
-public List<User> getUsers() {
-        return users;
-        }
-
-public void setUsers(List<User> users) {
-        this.users = users;
-        }
-
-@Override
-public boolean equals(Object o) {
+    @Override
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Role)) return false;
-
         Role role = (Role) o;
+        return Objects.equals(id, role.id) && Objects.equals(authority, role.authority);
+    }
 
-        if (getId() != null ? !getId().equals(role.getId()) : role.getId() != null) return false;
-        if (getAuthority() != null ? !getAuthority().equals(role.getAuthority()) : role.getAuthority() != null)
-        return false;
-        return getUsers() != null ? getUsers().equals(role.getUsers()) : role.getUsers() == null;
-        }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, authority);
+    }
 
-@Override
-public int hashCode() {
-        int result = getId() != null ? getId().hashCode() : 0;
-        result = 31 * result + (getAuthority() != null ? getAuthority().hashCode() : 0);
-        result = 31 * result + (getUsers() != null ? getUsers().hashCode() : 0);
-        return result;
-        }
-
-@Override
-public String toString() {
+    @Override
+    public String toString() {
         return "Role{" +
-        "id=" + id +
-        ", authority='" + authority + '\'' +
-        ", users=" + users +
-        '}';
-        }
+                "id=" + id +
+                ", authority='" + authority + '\'' +
+                '}';
+    }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -8,8 +8,8 @@ import ru.kata.spring.boot_security.demo.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-@Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.eMail = :eMail")
-User getByeMail(String eMail);
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.eMail = :eMail")
+    User getByeMail(String eMail);
 
     @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.username = :username")
     User getByUsername(String username);
